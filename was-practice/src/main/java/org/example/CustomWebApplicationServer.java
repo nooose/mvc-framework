2605cd71cfcdbd63f56897e8749ba2a59414a66e
nooose <hd15807@gmail.com>
2022-09-01 11:39:32 +0900

    feat: http requestLine 파싱 기능 구현
    method, path, queryString으로 분리

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -4,9 +4,10 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
     private final int port;
@@ -25,6 +26,16 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected.");
+
+                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while ((line = br.readLine())  != "") {
+                        System.out.println(line);
+                    }
+                }
             }
         }
     }
