d704f793f8f40c87a54f6ef7cf4e3be2183f1fc2
nooose <hd15807@gmail.com>
2022-09-02 18:04:39 +0900

    feat: 서블릿 구현

--- servlet/src/main/java/org/example/WebApplicationServletLauncher.java
+++ servlet/src/main/java/org/example/WebApplicationServletLauncher.java
@@ -1,9 +1,24 @@
 package org.example;
 
-import java.io.IOException;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class WebApplicationServletLauncher {
-    public static void main(String[] args) throws IOException {
+    private static final Logger logger = LoggerFactory.getLogger(WebApplicationServletLauncher.class);
 
+    public static void main(String[] args) throws Exception {
+        // 내장 톰캣
+        String webappDirLocation = "webapp/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
