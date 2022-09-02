09306f52952e0b70f1071674ab77d7f20fd9a42a
nooose <hd15807@gmail.com>
2022-09-02 10:58:53 +0900

    feat: Thread Pool을 이용하여 안정적으로 처리하도록 변경
    사용자 요청마다 새로운 Thread를 생성하여 처리했기 때문에
    리소스가 낭비되었다.

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -15,9 +15,12 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -34,7 +37,7 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected.");
 
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
