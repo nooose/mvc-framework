2fe14abff00a533e029fb8b63619fc1cabba53f1
nooose <hd15807@gmail.com>
2022-09-02 10:02:09 +0900

    fix: * 연산자 추가

--- was-practice/src/main/java/org/example/calculator/domain/Calculator.java
+++ was-practice/src/main/java/org/example/calculator/domain/Calculator.java
@@ -1,15 +1,12 @@
 package org.example.calculator.domain;
 
-import org.example.calculator.tobe.AdditionOperator;
-import org.example.calculator.tobe.ArithmeticOperator;
-import org.example.calculator.tobe.DivisionOperator;
-import org.example.calculator.tobe.SubtractionOperator;
+import org.example.calculator.tobe.*;
 
 import java.util.List;
 
 public class Calculator {
 
-    private static final List<ArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new DivisionOperator(), new SubtractionOperator());
+    private static final List<ArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
     /**
      * 간단한 사칙연산을 할 수 있다.
      * 양수로만 계산할 수 있다.
