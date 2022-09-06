77ba77a5166931c9c8d6bd61f6e812f990b68b0d
nooose <hd15807@gmail.com>
2022-09-06 23:12:23 +0900

    fix: 내장 톰캣 클래스 경로 지정

--- servlet/src/main/java/org/example/CalculatorServlet.java
+++ servlet/src/main/java/org/example/CalculatorServlet.java
@@ -7,19 +7,17 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet("/test")
+@WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        System.out.println("init");
         log.info("init");
     }
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        System.out.println("service");
         log.info("service");
     }
 
